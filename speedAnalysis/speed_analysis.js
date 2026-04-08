ea00a1d53546ccbd275c8aa3584583eca058623b
christianc-coder <crist200555gmail.com>
2026-04-08 11:44:28 -0400

    speed analysis

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -1,5 +1,5 @@
 let testText = "El zorro conchetumare salta encima del perro cojudo";
-let starTime, endTime; 
+let startTime, endTime; 
  function startTest(){
 
     // Pasa el texto de prueba al html  
@@ -9,4 +9,31 @@ let starTime, endTime;
     userInput.value = "";
     userInput.readOnly = false;
     userInput.focus();
+
+    startTime = new Date().getTime();
+}
+
+function endTest(){
+    
+    endTime = new Date().getTime();
+    document.getElementById("userInput").readOnly = true;
+    
+    const timeElapsed = (endTime - startTime) / 1000;
+    userTypedText = document.getElementById('userInput').value; 
+    const longitudText = userTypedText.length;
+    
+    const typeWords = userTypedText.split(/\s+/).filter( (m) => {return m !== ""}).length;
+
+    let wpm = 0;
+
+    if(timeElapsed !== 0 && !isNaN(typeWords)){
+        wpm = Math.round(( typeWords / timeElapsed) * 60);
+    }
+    
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = "<h2>Resultados de la Prueba</h2>" +
+    "<p>Longitud de Texto: " + longitudText + "</p>" +
+    "<p>Palabras escritas:" + typeWords + "</p>" +
+    "<p>Tiempo Transcurrido:" + timeElapsed.toFixed(2) + "segundos</p>" +
+    "<p>Palabras por minuto (WPM):" + wpm + "</p>";
 }
