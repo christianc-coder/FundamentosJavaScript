63dd9ed00381ee2d463fc95128b01c8fe105ab81
christianc-coder <crist200555@gmail.com>
2026-04-13 18:44:46 -0400

    hola

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -2,15 +2,15 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
-
+const delateTaskBtn = document.getElementById("delateTask");
 let tasks = [];
 
-function addTaskBtn (){
+function addTask(){
     const taskText = taskInput.value.trim();
     if ( taskText  !== "" ){
 
         // Empuja el objeto a un array
-        tasks.push({texto: taskText});
+        tasks.push({text: taskText, completed: false});
 
         //Limpia entrada
         taskInput.value = "";
@@ -19,13 +19,36 @@ function addTaskBtn (){
 } 
 
 function displayTasks(){
+
+    //Limpia la salida 
     taskList.innerHTML = "";
+    // Recorre cada elemento creando un elemento li y muestra info
     tasks.forEach((task,index) => {
         const li = document.createElement('li');
         li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
                     <label for="task-${index}">${task.text}</label>`;
-                    li.querySelector(input).addEventListener("change", () => toggleTask(index));
+                    li.querySelector("input").addEventListener("change", () => toggleTask(index));
                     taskList.appendChild(li);
 
     });
 } 
+
+function toggleTask(index){
+    tasks[index].completed = !tasks[index].completed;
+    displayTasks(); 
+}
+
+// Limpia las tareas completadas
+function clearCompletedTasks(){
+    tasks = tasks.filter( task => !task.completed);
+    displayTasks();
+}
+
+delateTaskBtn.addEventListener( "click", () => {
+   tasks.length = 0; 
+   displayTasks();
+});
+addTaskBtn.addEventListener("click", addTask);
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+
+
