47802d7f656fc0d6e9a6ff3fb458756c020fe223
christianc-coder <crist200555@gmail.com>
2026-04-24 18:59:16 -0400

    tarea

--- healthArticle/health_article.js
+++ healthArticle/health_article.js
@@ -5,8 +5,49 @@ xhr.open('GET', url, true);
 
 xhr.responseType = 'json';
 
+
 xhr.onload = function(){
-    const articles = xhr.response.article;
+    const articlesJson = xhr.response.articles;
     const articlesDiv = document.getElementById('articles');
     
+    articlesJson.forEach(function(article) {
+        const div = document.createElement("div");
+        const titulo = document.createElement("h2");
+        const descripcion = document.createElement("p");
+        const listHeader = document.createElement("h3");
+        const ul = document.createElement("ul");
+
+        div.classList.add('article');
+        titulo.textContent = article.title;
+        descripcion.textContent = article.description;
+        listHeader.textContent = "Consejos para lograrlo";
+
+        article.ways_to_achieve.forEach(function(recomendaciones){
+            const li = document.createElement('li');
+            li.textContent = recomendaciones;
+            ul.appendChild(li);
+        });
+
+        const beneficiosHeader = document.createElement("h3");
+        beneficiosHeader.textContent = "Beneficios";
+
+        const ul2 = document.createElement('ul');
+        article.benefits.forEach((benefit) => {
+            const li = document.createElement('li');
+            li.textContent = benefit;
+            ul2.appendChild(li);
+        });
+        
+
+        div.appendChild(titulo);
+        div.appendChild(descripcion);
+        div.appendChild(listHeader);
+        div.appendChild(ul);
+        div.appendChild(beneficiosHeader);
+        div.appendChild(ul2);
+
+        articlesDiv.appendChild(div);
+    });
 }
+
+xhr.send();
