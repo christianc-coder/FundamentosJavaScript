da14a4eed8867af7c3a84b25cb3d1344d37bf789
christianc-coder <crist200555@gmail.com>
2026-04-27 11:49:50 -0400

    Nuevo actualizacion

--- healthArticle/health_article.js
+++ healthArticle/health_article.js
@@ -53,17 +53,40 @@ xhr.onload = function(){
 xhr.send();
 
 const xhr2 = new XMLHttpRequest();
-const url2 = './health_article.json';
+const url2 = 'noticias.json';
 
 xhr2.open('GET', url2, true);
 
 xhr2.responseType = 'json';
 
 xhr2.onload = function(){
+
+    const NoticiaScreen = document.getElementById("articlesNoticias");
     const NoticiasJson = xhr2.response.noticias;
-    NoticiasJson.forEach(function(){})
+
+    NoticiasJson.forEach(function(Noticias){
+        const div = document.createElement("div");
+        const titulo = document.createElement("h2");
+        const reportero = document.createElement("p");
+        const descripcion = document.createElement("p");
+        const fecha = document.createElement("p");
+         
+        titulo.textContent = Noticias.titulo
+        reportero.textContent = Noticias.reportero;
+        descripcion.textContent = Noticias.descripcion;
+        fecha.textContent = Noticias.fecha;
+
+        div.appendChild(titulo);
+        div.appendChild(reportero);
+        div.appendChild(descripcion);
+        div.appendChild(fecha);
+
+        NoticiaScreen.appendChild(div);    
+
+    })
     
 }
+xhr2.send();
 
 
 
