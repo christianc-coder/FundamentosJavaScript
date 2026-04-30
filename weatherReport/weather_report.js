1985e9d4c66c5466e7be154191ea3f19a8dfeb43
christianc-coder <crist200555@gmail.com>
2026-04-30 11:45:15 -0400

    hola

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,16 +1,23 @@
-function showweatherDetails (event){
+  function showWeatherDetails (event){
+    
     event.preventDefault();
-  const cityInput = document.getElementById('city').value;
-  const ApiKey = 'a44c411dfafb4c1f8e3161251262904';
-  const ApiUrl = 'http://api.weatherapi.com/v1'
+    const cityInput = document.getElementById('city').value;
+    const infoEnPantalla = document.getElementById('weatherInfo');
+    const ApiKey = 'a44c411dfafb4c1f8e3161251262904';
+    const ApiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${ApiKey}&q=${cityInput}&days=5`;
+  
   
   fetch(ApiUrl)
-    .then( reponse => reponse.json())
+    .then( response => response.json())
     .then( data => {
-        const infoDeClima = document.getElementById('weatherInfo');
-        infoDeClima.innerHTML = `<h2> clima en ${data.name}</h2>
-        <p>Temperature: ${data.main.temp} &#8451;</p>
-        <p>Weather: ${data.weather[0].description}</p>`;
+        infoEnPantalla.innerHTML = `<h2> clima en ${data.location.name}</h2>
+        <p>Temperature: ${data.current.temp_c}</p>
+        <p>Weather: ${data.current.condition.text}</p>`;
+   })
+    .catch(error => {
+        infoEnPantalla.textContent = "Hubo un error";
+        console.error("Hubo un error", error);
+
    });
-}
-    
+};
+document.getElementById('weatherForm').addEventListener('submit', showWeatherDetails);  
