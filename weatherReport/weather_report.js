42bda019fbad7fad3622eaa2c03387220209c8e7
christianc-coder <crist200555@gmail.com>
2026-05-01 10:22:58 -0400

    funcionando

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -15,7 +15,7 @@
         <p>Clima: ${data.current.condition.text}</p>`;
    })
     .catch(error => {
-        infoEnPantalla.textContent = "Hubo un error";
+        infoEnPantalla.textContent = "Surgio un error";
         console.error("Hubo un error", error);
 
    });
@@ -29,21 +29,21 @@ function addLanLon(event){
     const lonInput = document.getElementById('lon').value;
     const lat_lonInfo = document.getElementById('lan&lonInfo');
 
-    apiKey2 = 'a44c411dfafb4xxxxxxxx';
-    apiUrl2 = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey2}&lat=${latInput}&lon=${lonInput}&days=5&lang=es`;
+    apiKey2 = 'a44c411dfafb4c1f8e3161251262904';
+    apiUrl2 = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey2}&q=${latInput},${lonInput}&days=5&lang=es`;
 
     fetch(apiUrl2)
     .then( response => response.json())
     .then( data => {
         lat_lonInfo.innerHTML = `
         <h2>Ubicacion: ${data.location.name}</h2>
-        <p>Salida de luna: ${data.astro.moonrise}</p>
-        <p>fase lunar: ${data.astro.moon_phase}</p>`;
+        <p>Salida de luna: ${data.forecast.forecastday[0].astro.moonrise}</p>
+        <p>Fase lunar: ${data.forecast.forecastday[0].astro.moon_phase}</p>`;
 
     })
     .catch( error => {
         console.error('hubo un error', error);
-        lat_lonInfo.textContent = 'surgio un error'
+        lat_lonInfo.textContent = 'Surgio un error'
     });
 
 }
